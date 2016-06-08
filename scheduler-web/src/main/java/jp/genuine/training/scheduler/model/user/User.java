3b78e94cb349d328c291cb4210bdb14887d00a26
JunyaNakamura <junya.nakamura@genuine-pt.jp>
2016-06-08 17:00:47 +0900

    Userクラスを実装しました。

--- scheduler-web/src/main/java/jp/genuine/training/scheduler/model/user/User.java
+++ scheduler-web/src/main/java/jp/genuine/training/scheduler/model/user/User.java
@@ -1,5 +1,27 @@
 package jp.genuine.training.scheduler.model.user;
 
 public class User {
+	private String value;
+
+	public User() {
+		value = "";
+	}
+
+	public User(String value) {
+		this.value = value;
+	}
+
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("User [value=%s]", value);
+	}
 
 }
